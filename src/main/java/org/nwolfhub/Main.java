a1caec9d013526fb259503be0eb23213bd042aa4
s0m31 <admin@nwolfhub.org>
2023-07-14 15:54:07 +0300

    Init project

---
+++ src/main/java/org/nwolfhub/Main.java
@@ -0,0 +1,27 @@
+package org.nwolfhub;
+
+import javax.swing.*;
+
+public class Main {
+    private JPanel notesWindow;
+    private JList notesList;
+    private JLabel webStatus;
+    private JPanel content;
+    private JButton switchWebState;
+    private JTextField noteName;
+    private JTextPane noteText;
+    private JButton newNote;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Nwolfhub notes");
+        frame.setContentPane(notesWindow);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 800);
+        frame.setVisible(true);
+    }
+}
