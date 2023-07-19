16099c4fea91146b81648e0860686b91f3bbe63f
s0m31 <admin@nwolfhub.org>
2023-07-19 16:39:28 +0300

    Finish go launcher

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module nwolfhubNotesLauncher
 
 go 1.20
+
+require github.com/walle/targz v0.0.0-20140417120357-57fe4206da5a // indirect
--- go/main.go
+++ go/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"github.com/walle/targz"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -11,6 +14,13 @@ var javaBase string
 
 func main() {
 	locateJava()
+	log.Println("Located Java")
+	notes := exec.Command(javaBase+"/bin/java", "-jar", "notes.jar")
+	log.Println(javaBase)
+	notes.Stdout = os.Stdout
+	notes.Stdin = os.Stdin
+	notes.Stderr = os.Stderr
+	notes.Run()
 }
 
 func locateJava() {
@@ -25,13 +35,10 @@ func locateJava() {
 				}
 			}
 			if javaBase == "" {
-				fmt.Println()
-				fmt.Println("Did not detect installed java! Downloading openjdk 11")
+				log.Println("Java not found! Downloading openjdk11")
 				prevFile, err := os.Stat("./openjava.tar.gz")
 				if err != nil {
-					if os.IsNotExist(err) {
-
-					} else {
+					if !os.IsNotExist(err) {
 						log.Fatal(err)
 					}
 				} else {
@@ -40,9 +47,31 @@ func locateJava() {
 						log.Fatal(err)
 					}
 				}
+				file, err := os.Create("./openjava.tar.gz")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
+				resp, err := http.Get("https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz")
+				defer resp.Body.Close()
+				_, err = io.Copy(file, resp.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Println("Extracting archive")
+				err = targz.Extract("./openjava.tar.gz", ".")
+				if err != nil {
+					log.Fatal(err)
+				}
+				exec.Command("chmod", "+x", "jdk-11.0.2", "-R").Run()
+				javaBase = "./jdk-11.0.2"
+				log.Println("Cleaning up")
+				os.Remove("openjava.tar.gz")
 			}
 		} else {
 			log.Fatal(err)
 		}
+	} else {
+		javaBase = loc
 	}
 }
